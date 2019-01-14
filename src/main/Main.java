c09d536b1d8de444920e6126604b8854702c7e42
Xusha98 <xeniafedorov@web.de>
2019-01-14 18:54:23 +0100

    Verzweiflung pur, komme nicht hinter das Problem mit den Objekten

--- src/main/Main.java
+++ src/main/Main.java
@@ -40,7 +40,7 @@ public class Main {
     	window.lockMouse();
     	shader.create();
 
-    	setBackground();
+    	//setBackground();
 
         TexturedModel model = new TexturedModel(new float[] {
         		-1.0f, 1.0f, -1.0f, //V0
@@ -105,11 +105,11 @@ public class Main {
         						19, 17, 18,
         						20, 21, 23,
         						23, 21, 22
-        						}, "beautiful.png");
+        						}, "blue.png");
         allModels.add(model);
 
-        ModelEntity entity = new ModelEntity(model, new Vector3f(5, 10, 5), new Vector3f(0, 0, 0), new Vector3f(1, 1, 1));
-        blockList.add(entity);
+        ModelEntity entity = new ModelEntity(model, new Vector3f(1, 1, 1), new Vector3f(0, 0, 0), new Vector3f(1, 1, 1));
+        //blockList.add(entity);
 
 
         TexturedModel model2 = new TexturedModel(new float[] {
@@ -175,11 +175,154 @@ public class Main {
         						19, 17, 18,
         						20, 21, 23,
         						23, 21, 22
-        						}, "beautiful.png");
+        						}, "green.png");
         allModels.add(model2);
 
-        ModelEntity entity2 = new ModelEntity(model2, new Vector3f(8, 5, 8), new Vector3f(0, 0, 0), new Vector3f(1, 1, 1));
-        blockList.add(entity2);
+        ModelEntity entity2 = new ModelEntity(model2, new Vector3f(3, 3, 3), new Vector3f(0, 0, 0), new Vector3f(1, 1, 1));
+        //blockList.add(entity2);
+        
+        TexturedModel model3 = new TexturedModel(new float[] {
+        		-1.0f, 1.0f, -1.0f, //V0
+        		-1.0f, -1.0f, -1.0f, //V1
+        		1.0f, -1.0f, -1.0f, //V2
+        		1.0f, 1.0f, -1.0f, //V3
+        		-1.0f, 1.0f, 1.0f, //V4
+        		-1.0f, -1.0f, 1.0f, //V5
+        		1.0f, -1.0f, 1.0f, //V6
+        		1.0f, 1.0f, 1.0f, //V7
+        		1.0f, 1.0f, -1.0f, //V3
+        		1.0f, -1.0f, -1.0f, //V2
+        		1.0f, -1.0f, 1.0f, //V6
+        		1.0f, 1.0f, 1.0f, //V7
+        		-1.0f, 1.0f, -1.0f, //V0
+        		-1.0f, -1.0f, -1.0f, //V1
+        		-1.0f, -1.0f, 1.0f, //V5
+        		-1.0f, 1.0f, 1.0f, //V4
+        		-1.0f, 1.0f, 1.0f, //V4
+        		-1.0f, 1.0f, -1.0f, //V0
+        		1.0f, 1.0f, -1.0f, //V3
+        		1.0f, 1.0f, 1.0f, //V7
+        		-1.0f, -1.0f, 1.0f, //V5
+        		-1.0f, -1.0f, -1.0f, //V1
+        		1.0f, -1.0f, -1.0f, //V2
+        		1.0f, -1.0f, 1.0f //V6
+        		}, new float[]{
+        				0f, 0f,
+        				0f, 1f,
+        				1f, 1f,
+        				1f, 0f,
+        				0f, 0f,
+        				0f, 1f,
+        				1f, 1f,
+        				1f, 0f,
+        				0f, 0f,
+        				0f, 1f,
+        				1f, 1f,
+        				1f, 0f,
+        				0f, 0f,
+        				0f, 1f,
+        				1f, 1f,
+        				1f, 0f,
+        				0f, 0f,
+        				0f, 1f,
+        				1f, 1f,
+        				1f, 0f,
+        				0f, 0f,
+        				0f, 1f,
+        				1f, 1f,
+        				1f, 0f
+        				}, new int[]{
+        						0, 1, 3,
+        						3, 1, 2,
+        						4, 5, 7,
+        						7, 5, 6,
+        						8, 9, 11,
+        						11, 9, 10,
+        						12, 13, 15,
+        						15, 13, 14,
+        						16, 17, 19,
+        						19, 17, 18,
+        						20, 21, 23,
+        						23, 21, 22
+        						}, "yellow.png");
+        allModels.add(model3);
+
+        ModelEntity entity3 = new ModelEntity(model3, new Vector3f(5, 5, 5), new Vector3f(0, 0, 0), new Vector3f(1, 1, 1));
+        //blockList.add(entity3);
+        
+        TexturedModel model4 = new TexturedModel(new float[] {
+        		-1.0f, 1.0f, -1.0f, //V0
+        		-1.0f, -1.0f, -1.0f, //V1
+        		1.0f, -1.0f, -1.0f, //V2
+        		1.0f, 1.0f, -1.0f, //V3
+        		-1.0f, 1.0f, 1.0f, //V4
+        		-1.0f, -1.0f, 1.0f, //V5
+        		1.0f, -1.0f, 1.0f, //V6
+        		1.0f, 1.0f, 1.0f, //V7
+        		1.0f, 1.0f, -1.0f, //V3
+        		1.0f, -1.0f, -1.0f, //V2
+        		1.0f, -1.0f, 1.0f, //V6
+        		1.0f, 1.0f, 1.0f, //V7
+        		-1.0f, 1.0f, -1.0f, //V0
+        		-1.0f, -1.0f, -1.0f, //V1
+        		-1.0f, -1.0f, 1.0f, //V5
+        		-1.0f, 1.0f, 1.0f, //V4
+        		-1.0f, 1.0f, 1.0f, //V4
+        		-1.0f, 1.0f, -1.0f, //V0
+        		1.0f, 1.0f, -1.0f, //V3
+        		1.0f, 1.0f, 1.0f, //V7
+        		-1.0f, -1.0f, 1.0f, //V5
+        		-1.0f, -1.0f, -1.0f, //V1
+        		1.0f, -1.0f, -1.0f, //V2
+        		1.0f, -1.0f, 1.0f //V6
+        		}, new float[]{
+        				0f, 0f,
+        				0f, 1f,
+        				1f, 1f,
+        				1f, 0f,
+        				0f, 0f,
+        				0f, 1f,
+        				1f, 1f,
+        				1f, 0f,
+        				0f, 0f,
+        				0f, 1f,
+        				1f, 1f,
+        				1f, 0f,
+        				0f, 0f,
+        				0f, 1f,
+        				1f, 1f,
+        				1f, 0f,
+        				0f, 0f,
+        				0f, 1f,
+        				1f, 1f,
+        				1f, 0f,
+        				0f, 0f,
+        				0f, 1f,
+        				1f, 1f,
+        				1f, 0f
+        				}, new int[]{
+        						0, 1, 3,
+        						3, 1, 2,
+        						4, 5, 7,
+        						7, 5, 6,
+        						8, 9, 11,
+        						11, 9, 10,
+        						12, 13, 15,
+        						15, 13, 14,
+        						16, 17, 19,
+        						19, 17, 18,
+        						20, 21, 23,
+        						23, 21, 22
+        						}, "red.png");
+        allModels.add(model4);
+
+        ModelEntity entity4 = new ModelEntity(model4, new Vector3f(7, 7, 7), new Vector3f(0, 0, 0), new Vector3f(1, 1, 1));
+        //blockList.add(entity3);
+        
+        background.add(entity);
+        background.add(entity2);
+        background.add(entity3);
+        background.add(entity4);
 
         while (!window.closed()) {
         	if (window.isUpdating()) {
@@ -210,6 +353,13 @@ public class Main {
 
 
         		shader.bind();
+        		
+//        		for(int i = 1; i < background.size(); i++) {
+//        			ModelEntity me = background.get(i);
+//        			renderer.renderModelEntity(me);
+//        		}
+//        		ModelEntity em = background.get(0);
+//        		renderer.renderModelEntity(em);
 
         		for(ModelEntity me : background) {
         			renderer.renderModelEntity(me);
@@ -217,10 +367,12 @@ public class Main {
         		}
         		if(!blockList.isEmpty()) {
         			for(ModelEntity me : blockList) {
-		            	renderer.renderModelEntity(me);
+        				renderer.renderModelEntity(me);
 		            	//System.out.println("Block VertexArrayID: "+me.getModel().getVertexArrayID()+" Position: "+me.getPosition().getX()+", "+me.getPosition().getY()+", "+me.getPosition().getZ());
 		            }
         		}
+        		
+        		System.out.println("X: "+background.get(0).getPosition().getX()+"; Y: "+background.get(0).getPosition().getY()+"; Z: "+background.get(0).getPosition().getZ());
 	            shader.unbind();
 	            window.swapBuffers();
         	}
@@ -294,6 +446,7 @@ public class Main {
     	        background.add(entity);
     		}
     	}
+    	
 
     	/*				-1.0f, 1.0f, 0,  //TOP LEFT V0
     	        		1.0f, 1.0f, 0,  //TOP RIGHT V1
