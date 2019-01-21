7deee9896fc8a31ef7a54163564761dd25df4277
Vicky Lara <s0558484@htw-berlin.de>
2019-01-21 19:36:10 +0100

    Create Vector4f.java

---
+++ src/maths/Vector4f.java
@@ -0,0 +1,53 @@
+package maths;
+
+public class Vector4f {
+	
+	private float w, x, y, z;
+	public Vector4f() {
+		w = 0;
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+	
+	public Vector4f(float w, float x, float y, float z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public float getW() {
+		return w;
+	}
+
+	public void setW(float w) {
+		this.w = w;
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	public float getZ() {
+		return z;
+	}
+
+	public void setZ(float z) {
+		this.z = z;
+	}
+	
+	
+
+}
